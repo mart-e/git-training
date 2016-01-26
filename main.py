45bd2a2bdd2edddf260441248639b89e13a9a566
Martin Trigaux <mat@odoo.com>
2016-01-26 18:29:25 +0530

    [ADD] second and more efficiant implementation
    keep method `fibonacci` as wrapper to internal method

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python2
 
 
-def fibonacci(n):
+def _recursive_fin(n):
     """inefficiant implementation of fibonacci using recursion"""
     if n==1 or n==2:
         return 1
-    return fibonacci(n-1)+fibonacci(n-2)
+    return _recursive_fin(n-1)+_recursive_fin(n-2)
+
+
+def _looping_fin(n):
+    """more efficiant fibonacci implementation"""
+    a, b = 1, 1
+    for i in range(n-1):
+        a, b = b, a+b
+    return a
+
+
+def fibonacci(n):
+    """return nth number of the fibonacci sequence"""
+    return _looping_fin(n)
 
 if __name__ == '__main__':
     print fibonacci(30)
